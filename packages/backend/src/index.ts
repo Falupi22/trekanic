8f3651b24e556024ea067e2c4a352a64f9463183
Tal Frumkin <tal.frumkin@gmail.com>
2024-06-26 14:09:35 -0500

    add setup files

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,5 @@
+export function isEven(n: number): boolean {
+  return n % 2 === 0
+}
+
+console.log('Utils function isEven is imported successfully')
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { FooComponent } from "@monorepo/ui"
+import { isEven } from "@monorepo/utils"
+
+console.log(isEven(4))
+console.log("nine is even?", isEven(9))
+FooComponent()
